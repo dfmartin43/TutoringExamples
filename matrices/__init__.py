a117000a870ac262921859f59311a35086e8d4e0
dfmartin43 <58089053+dfmartin43@users.noreply.github.com>
2021-10-05 16:09:38 -0400

    Update __init__.py

--- matrices/__init__.py
+++ matrices/__init__.py
@@ -1,2 +1,15 @@
-def print_something():
-  print("Hello Tutoring")
+def connection_check():
+  print("Successful. This is version from 05Oct2021.")
+
+def problem1_description():
+  print("Two hotdogs and a hamburger cost $3.50, and one hotdog and 2 hamburgers cost $4.00. \n We want to know how much and individual hotdog and hamburger cost. \n Start by setting this up as a matrix problem.")
+  
+def get_matrices():
+  A = np.array([2, 1],[1, 2])
+  b = np.array([3.5],[4])
+  return A, b
+
+def calc_x(A, b):
+  x = np.matmul(np.linalg.inv(A),b)
+  return x
+  
