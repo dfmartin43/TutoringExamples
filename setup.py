cde1836149ce18806516f00fb7d2a011678b1e7b
dfmartin43 <58089053+dfmartin43@users.noreply.github.com>
2021-10-05 15:57:10 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,20 @@
-#from distutils.core import setup
-#setup(name='test',
-#      version='1.0',
-#      packages=['matrices'],
-#      intall_requires=['numpy'],
-#      )
-
-from setuptools import setup
-
-setup(name='test',
-      version='0.1',
-      description='The funniest joke in the world',
-      url='http://github.com/dfmartin43/test',
-      #author='Flying Circus',
-      #author_email='flyingcircus@example.com',
-      #license='MIT',
+from distutils.core import setup
+setup(name='TutoringExamples',
+      version='1.0',
       packages=['matrices'],
-      #zip_safe=False
-      install_requires=['numpy']
+      intall_requires=['numpy'],
       )
+
+#from setuptools import setup
+
+#setup(name='TutoringExamples',
+#      version='0.1',
+#      description='The funniest joke in the world',
+#      url='http://github.com/dfmartin43/test',
+#      #author='Flying Circus',
+#      #author_email='flyingcircus@example.com',
+#      #license='MIT',
+#      packages=['matrices'],
+#      #zip_safe=False
+#      install_requires=['numpy']
+#      )
