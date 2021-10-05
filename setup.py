9719a276e6d57a7f576a845d7a5406dd99bbaa9e
dfmartin43 <58089053+dfmartin43@users.noreply.github.com>
2021-10-05 15:59:25 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -11,10 +11,10 @@ setup(name='TutoringExamples',
       version='0.1',
       description='The funniest joke in the world',
       url='http://github.com/dfmartin43/test',
-      #author='Flying Circus',
-      #author_email='flyingcircus@example.com',
-      #license='MIT',
+      author='Flying Circus',
+      author_email='flyingcircus@example.com',
+      license='MIT',
       packages=['matrices'],
-      #zip_safe=False
+      zip_safe=False,
       install_requires=['numpy']
       )
